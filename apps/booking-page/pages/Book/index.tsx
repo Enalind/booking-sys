4ebc479a20d55a3f7c93f0b0a5fb9d7bac14dd1b
Elias Lindström <enalindstrom@gmail.com>
2023-03-11 12:10:55 +0100

    Added app dir

--- apps/booking-page/pages/Book/index.tsx
+++ apps/booking-page/pages/Book/index.tsx
@@ -1,5 +1,30 @@
 
+import { getFirestore, doc, getDoc, collection, query, where, getDocs } from "firebase/firestore"
+import {firebase, firestore, env} from "../../utils/firebase"
+import { useEffect, useState } from 'react';
 
-export default function Book(){
+function Book({data}){
+    const [Docs, GetDocs] = useState([])
+    async function getDocuments(){
+        const docRef = await getDocs(collection(firestore, "Calendars"))
+        docRef.forEach(doc => {
+            console.log(doc.data)
+        })
+        return docRef.docs
+    }
+
+    useEffect(() => {
+        getDocuments().then(res => console.log(res))
+        console.log(data)
+    }, [])
     
+    return<p>Hello</p>
 }
+
+export async function getServerSideProps(){
+    
+    const data = env
+    return {props: {data}}
+}
+
+export default Book
--- apps/booking-page/utils/firebase.ts
+++ apps/booking-page/utils/firebase.ts
@@ -1,5 +1,6 @@
 import {initializeApp} from "firebase/app"
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
 
 
 const config = {
@@ -12,7 +13,17 @@ const config = {
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
-
+const firebaseConfig = {
+    apiKey: "AIzaSyDUISKPg4mWVFmvuH_PgWgR_iFyiKfG42Q",
+    authDomain: "booking-32922.firebaseapp.com",
+    projectId: "booking-32922",
+    storageBucket: "booking-32922.appspot.com",
+    messagingSenderId: "1057868823401",
+    appId: "1:1057868823401:web:722aadb8e6ea24ecce1bf8",
+    measurementId: "G-RRY1MY481F"
+  };
 
 export const firebase = initializeApp(config);
-export const analytics = getAnalytics(firebase);
+export const firestore = getFirestore(firebase)
+export const env = [config, firebaseConfig]
+// export const analytics = getAnalytics(firebase);
