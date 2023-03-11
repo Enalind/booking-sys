10e270274581478449f1956aab8d953226914a74
Elias Lindström <enalindstrom@gmail.com>
2023-03-11 21:24:25 +0100

    Added firebase storage and firebase firestore to Booking page

--- apps/booking-page/next.config.js
+++ apps/booking-page/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental:{appDir: true}
+  experimental:{
+    appDir: true,
+    allowFutureImage: true
+  },
+  images: {
+    domains: ["firebasestorage.googleapis.com"]
+  }
 }
 
 module.exports = nextConfig
+  
+
--- apps/booking-page/utils/firebase.ts
+++ apps/booking-page/utils/firebase.ts
@@ -1,7 +1,7 @@
 import {initializeApp} from "firebase/app"
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-
+import {getStorage} from "firebase/storage"
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -13,17 +13,9 @@ const config = {
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDUISKPg4mWVFmvuH_PgWgR_iFyiKfG42Q",
-    authDomain: "booking-32922.firebaseapp.com",
-    projectId: "booking-32922",
-    storageBucket: "booking-32922.appspot.com",
-    messagingSenderId: "1057868823401",
-    appId: "1:1057868823401:web:722aadb8e6ea24ecce1bf8",
-    measurementId: "G-RRY1MY481F"
-  };
+
 
 export const firebase = initializeApp(config);
 export const firestore = getFirestore(firebase)
-export const env = [config, firebaseConfig]
+export const storage = getStorage(firebase)
 // export const analytics = getAnalytics(firebase);
