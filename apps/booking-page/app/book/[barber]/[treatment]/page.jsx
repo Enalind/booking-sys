631848cb3ee349915a18f9b942282b851bc2b2e4
Elias Lindström <enalindstrom@gmail.com>
2023-03-14 20:38:40 +0100

    Calculated time ranges correctly

--- apps/booking-page/app/book/[barber]/[treatment]/page.jsx
+++ apps/booking-page/app/book/[barber]/[treatment]/page.jsx
@@ -2,10 +2,11 @@ import {firestore} from "../../../../utils/firebase"
 import {collection, getDocs, doc, getDoc } from "firebase/firestore"
 import { Koulen } from "next/font/google";
 import moment from "moment"
+import { extendMoment } from 'moment-range';
 import styles from "./styles.module.css"
 const koulen = Koulen({weight: "400", subsets: ["latin"]});
+const extendedMoment = extendMoment(moment)
 async function getStartEnd(barber, date){
-    console.log(barber)
     const calendarDocSnap = await getDocs(collection(firestore, `Calendars/${barber}/${date.toString()}`))
     for (const i in calendarDocSnap.docs){
         const doc = calendarDocSnap.docs[i]
@@ -14,14 +15,49 @@ async function getStartEnd(barber, date){
         }
     }
 }
-async function getBreaks(date){
-    const calendarDocSnap = await getDoc(doc(firestore, "Calendars", barber, date.toString(), "Breaks"))
-    console.log(calendarDocSnap.data())
+async function getBreaks(barber, date){
+    var breaks = []
+    const calendarDocSnap = await getDoc(doc(firestore, `Calendars/${barber}/${date.toString()}/Breaks`))
+    Object.values(calendarDocSnap.data()).forEach(data => {
+        breaks.push(data.map(dataItem => moment(dataItem, "HH:mm")))
+    })
+    const bookings = await getDocs(collection(firestore, `Calendars/${barber}/${date.toString()}/Breaks/Bookings`))
+    for(const i in bookings.docs){
+        const booking = bookings.docs[i]
+        breaks.push(booking.data()["StartStop"].map(dataItem => moment(dataItem, "HH:mm")))
+    }
+    return breaks
+}
+function getFreeTimes(startEnd, breaks, treatmentTime){
+    var initialTime = moment(startEnd.Start, "HH:mm")
+    var stopTime = moment(startEnd.End, "HH:mm")
+    var availiableTimes = []
+    
+    while(initialTime.isBefore(stopTime)){
+        var boolArray = []
+        var range = extendedMoment.range(initialTime, initialTime.clone().add(treatmentTime-1, "minutes"))
+        breaks.forEach(singleBreak => {
+            const breakRange = extendedMoment.range(singleBreak[0], singleBreak[1])
+            console.log(breakRange, range, range.overlaps(breakRange, {adjacent: false}))
+            boolArray.push(range.overlaps(breakRange, {adjacent: false}))
+        })
+        if(boolArray.every(e => e === false)){
+            availiableTimes.push(initialTime.clone())
+        }
+        initialTime.add(treatmentTime, "minutes")
+        
+    }
+    return availiableTimes
+}
+async function getTreatmentTime(barber, treatment){
+    const treatmentDocSnap = await getDoc(doc(firestore, `Calendars/${barber}/Treatments/${treatment}`))
+    return treatmentDocSnap.data()["Time"]
 }
 export default async function Calendar({params}){
-    console.log(typeof params.barber)
-    var StartEnd = await getStartEnd(params.barber, moment(moment().subtract(1, "days")).format("YYYYMMDD"))
-    await getBreaks()
-
+    var startEnd = await getStartEnd(params.barber, moment(moment().subtract(1, "days")).format("YYYYMMDD"))
+    var breaks = await getBreaks(params.barber, moment(moment().subtract(1, "days")).format("YYYYMMDD"))
+    var treatmentTime = await getTreatmentTime(params.barber, params.treatment)
+    var freeTimes = getFreeTimes(startEnd, breaks, treatmentTime)
+    console.log(freeTimes.map(freeTime => freeTime.format("HH:mm")))
     return<p>Hello</p>
 }
